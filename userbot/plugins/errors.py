b07b4c9a43f3341561997402f8e43b19e0e28e50
JAISHNAVPRASAD <62203009+JAISHNAVPRASAD-DarklIous@users.noreply.github.com>
2020-05-31 11:47:11 +0530

    kang from PhoeniX

---
+++ userbot/plugins/errors.py
@@ -0,0 +1,31 @@
+# Exclusive for PhoeniX
+#Ported To DeOXy
+# By @Techy05
+# Credits --> @authoritydmc and Priyam Kalra
+# You should not use this module without proper credits
+# A dictionary of errors. With solutions ofc.
+# Syntax (.errors)
+
+from telethon import events
+from userbot.utils import admin_cmd
+import asyncio
+from telethon.tl import functions, types
+from global_variables_sql import ERROR_LIST
+
+
+@borg.on(admin_cmd(pattern="errors ?(.*)"))
+async def _(event):
+    if event.fwd_from:
+        return
+    erros = "💫  **List of known errors in Userbots (for noobs)**\n"
+    ERROR_LIST.sort()
+    prev = "1"
+    num = 0
+    for err in ERROR_LIST:
+        num += 1
+        if prev[0] != err[0]:
+            erros += f"\n\t{err[0].upper()}\n"
+        erros += f"-  ```{err}```\n"
+        prev = err
+    erros += f"\n\n__Number of solutions loaded:__ {num}\n**Tip:** Use .solution <error_name> for more info."
+    await event.edit(erros)
