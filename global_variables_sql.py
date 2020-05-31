d22aec83c4b693159db7b538a8721c6d139ec3bf
JAISHNAVPRASAD <62203009+JAISHNAVPRASAD-DarklIous@users.noreply.github.com>
2020-05-31 11:42:05 +0530

    Update global_variables_sql.py

--- global_variables_sql.py
+++ global_variables_sql.py
@@ -6,8 +6,12 @@ PACK_NAME = Var.PACK_NAME
 ANIM_PACK_NAME = Var.ANIM_PACK_NAME
 PACKS = Var.PACKS_CONTENT
 DL = Var.TEMP_DOWNLOAD_DIRECTORY
-# add modules to this list using MODULES_LIST.append(MODULE_NAME)
+# add modules to this list using MODULE_LIST.append(MODULE_NAME)
 MODULE_LIST = []
+# add error to this list using ERROR_LIST.append(ERROR_NAME)
+ERROR_LIST = []
 # add syntax to this dictionary using SYNTAX.update()
 SYNTAX = {}
+# add errors to this dictionary using ERROR.update()
+ERROR = {}
 BUILD = "USER-49x03"
