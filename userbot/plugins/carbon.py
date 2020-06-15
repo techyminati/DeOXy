7b4da2ad8b9e6cd6f07544fd45d9c52c2faf07c5
JAISHNAVPRASAD <62203009+JAISHNAVPRASAD-DarklIous@users.noreply.github.com>
2020-06-15 19:36:52 +0530

    oof

--- userbot/plugins/carbon.py
+++ userbot/plugins/carbon.py
@@ -84,7 +84,7 @@ async def carbon_api(e):
    await e.client.send_file(
          e.chat_id,
          file,
-         caption="<< `Here's your new DP !!` \n **Carbonised by** [𝔡𝓔𝐎𝕩𝕐 USERBOT](https://github.com/JAISHNAVPRASAD-DarklIous/DeOXy)>>,
+         caption="`Here's your new DP !!` \n **Carbonised by** [𝔡𝓔𝐎𝕩𝕐 USERBOT](https://github.com/JAISHNAVPRASAD-DarklIous/DeOXy)",
          force_document=True,
          reply_to=e.message.reply_to_msg_id
          )
