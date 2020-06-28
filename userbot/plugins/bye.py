1c9de9c497149bd7d5d270ff5f308eff6e79cea2
JAISHNAVPRASAD <62203009+JAISHNAVPRASAD-DarklIous@users.noreply.github.com>
2020-06-28 11:00:08 +0530

    Update bye.py

--- userbot/plugins/bye.py
+++ userbot/plugins/bye.py
@@ -2,31 +2,31 @@
 # Courtesy @yasirsiddiqui
 
 """
-.bye
+#syntax .leave
 """
 from telethon.tl.functions.channels import LeaveChannelRequest
 from userbot.utils import admin_cmd
 from global_variables_sql import SYNTAX, MODULE_LIST
 import time
 
-MODULE_LIST.append("bye")
+MODULE_LIST.append("leave")
 
 @borg.on(admin_cmd("bye", outgoing=True))
 async def leave(e):
     if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
-        await e.edit("`I am leaving this chat.....!`")
+        await e.edit("`Chat Leave Request Proceeded`")
         time.sleep(3)
         if '-' in str(e.chat_id):
             await borg(LeaveChannelRequest(e.chat_id))
         else:
-            await e.edit('`Sir This is Not A Chat`')
+            await e.edit('`Unable To Leave - Not A Chat`')
 
 
 SYNTAX.update({
-    "bye": f"\
+    "Leave": f"\
 **Requested Module --> Chat Leaver**\
 \n\nDetailed usage of fuction(s):\
-\n\n```.bye```\
+\n\n```.Leave```\
 \nUsage: Leaves The Current Chat/Group.\
 "
 })            
