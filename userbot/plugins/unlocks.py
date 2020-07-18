a58301979c390f429b11430460eb3d6cac298fb2
JAISHNAVPRASAD <62203009+JAISHNAVPRASAD-DarklIous@users.noreply.github.com>
2020-07-18 07:56:19 +0530

    Update unlocks.py

--- userbot/plugins/unlocks.py
+++ userbot/plugins/unlocks.py
@@ -2,20 +2,21 @@
 # By Priyam Kalra
 
 import os
-from userbot import syntax
+from global_variables_sql import SYNTAX, MODULE_LIST
 
+MODULE_LIST.append("unlocks")
 
-@bot.on(command(func=lambda e: e.is_group))
+@borg.on(command(func=lambda e: e.is_group))
 async def watch(event):
-    if str(event.chat_id) not in str(Config.UNLOCKED_CHATS):
+    if str(event.chat_id) not in str(Var.UNLOCKED_CHATS):
         return
-    if not os.path.isdir(Config.DOWNLOAD_DIRECTORY):
-        os.makedirs(Config.DOWNLOAD_DIRECTORY)
+    if not os.path.isdir(Var.DOWNLOAD_DIRECTORY):
+        os.makedirs(Var.DOWNLOAD_DIRECTORY)
     if event.sticker:
         reply = await event.get_reply_message()
         file = event.sticker
         file_name = "sticker.png"
-        to_download_directory = Config.DOWNLOAD_DIRECTORY
+        to_download_directory = Var.DOWNLOAD_DIRECTORY
         downloaded_file_name = os.path.join(to_download_directory, file_name)
         downloaded_file_name = await bot.download_media(
             file,
