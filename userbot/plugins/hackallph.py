ca84c33e047d4af4e13baa0fb08123046ae505d4
JAISHNAVPRASAD <62203009+JAISHNAVPRASAD-DarklIous@users.noreply.github.com>
2020-07-21 23:09:05 +0530

    Update hackallph.py

--- userbot/plugins/hackallph.py
+++ userbot/plugins/hackallph.py
@@ -32,6 +32,7 @@ async def _(event):
     if input_str == "hackallph":
 
         await event.edit(input_str)
+        
     if event.reply_to_msg_id:
         reply_message = await event.get_reply_message()
         replied_user = await event.client(GetFullUserRequest(reply_message.from_id))
