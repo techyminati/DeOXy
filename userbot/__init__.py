94141c35f8f3a30a8d2f378c259825f0dc574df3
TechyNewbie <63805243+Techy05@users.noreply.github.com>
2020-08-17 22:12:24 +0530

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -133,6 +133,9 @@ if bool(ENV):
     BIO_PREFIX = os.environ.get("BIO_PREFIX", None)
     DEFAULT_BIO = os.environ.get("DEFAULT_BIO", None)
 
+    # Unlock Stickers
+    UNLOCKED_CHATS = os.environ.get("UNLOCKED_CHATS", None)
+
     LASTFM_API = os.environ.get("LASTFM_API", None)
     LASTFM_SECRET = os.environ.get("LASTFM_SECRET", None)
     LASTFM_USERNAME = os.environ.get("LASTFM_USERNAME", None)
