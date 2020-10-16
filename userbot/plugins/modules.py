b23d4091f92dc1483a7c124fb616d5c5dd21c48b
Techy05 <63805243+Techy05@users.noreply.github.com>
2020-10-16 09:52:04 +0530

    Test: HTML parse mode

--- userbot/plugins/modules.py
+++ userbot/plugins/modules.py
@@ -7,17 +7,16 @@ async def modulelist(event):
     if event.fwd_from:
         return
     modcount = 1
-    msg = "**Available Modules:**"
+    msg = "<strong>Available Modules:</strong>"
     title = ""
     for key in sorted(HELPER, key=lambda e: e.upper()):
-        new_title = f"\n\n**{key[0].upper()}**\n"
+        new_title = f"\n\n<strong>{key[0].upper()}</strong>\n"
         if new_title == title:
             title = "\n"
         else:
             title = new_title
-        msg += f"{title}• {key}"
+        msg += f"{title}• <code>{key}</code>"
         title = new_title
         modcount += 1
-    msg += f"\n\nNumber of modules: **{modcount}**\nSend .help <module_name> to get help regarding a module."
-    await event.edit(msg)
-    
+    msg += f"\n\nNumber of modules: <strong>{modcount}</strong>\nSend <code>.help <module_name></code> to get help regarding a module."
+    await event.edit(msg, parse_mode="HTML")
